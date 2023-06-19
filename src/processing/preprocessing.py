b059360aa37dae0e5deea7e74d5d77201598df69
Colin Daglish <colin.daglish@ons.gov.uk>
2023-06-19 16:45:39 +0100

    Tighten load config

--- src/processing/preprocessing.py
+++ src/processing/preprocessing.py
@@ -1,7 +1,8 @@
 import yaml
 
-def load_config(filepath:str):
-    ''' Loads configuration settings from given filepath to
+
+def load_config(filepath: str):
+    """Loads configuration settings from given filepath to
     yaml file
 
     Parameters
@@ -13,14 +14,10 @@ def load_config(filepath:str):
     -------
     dict
         the configuration settings with key-value pairs
-    '''
-    if type(filepath) is not str: 
+    """
+    if type(filepath) is not str:
         raise TypeError("filepath must be a string")
-    
-    with open(filepath) as file:
-        try:
-            config = yaml.safe_load(file)
-            return config
-        except yaml.YAMLError as exception:
-            print(exception)
-    
+
+    with open(filepath, "r") as file:
+        config = yaml.safe_load(file)
+    return config
