669fdad57cecd0f5ec030ad9782da0f3b7c9b559
Colin Daglish <colin.daglish@ons.gov.uk>
2023-07-05 17:14:39 +0100

    Add test remove blanks functions

--- tests/processing/test_preprocessing.py
+++ tests/processing/test_preprocessing.py
@@ -1,6 +1,10 @@
-import pytest
+import unittest
 
-from src.processing import preprocessing
+import numpy as np
+import pytest
+from pandas import Series
+
+from src.processing.preprocessing import _replace_blanks, load_config, remove_blank_rows
 
 
 class TestLoadConfig:
@@ -8,7 +12,7 @@ class TestLoadConfig:
         """test for assertion error"""
         bad_input = 123
         with pytest.raises(Exception) as e_info:
-            preprocessing.load_config(bad_input)
+            load_config(bad_input)
         assert (
             str(e_info.value) == "filepath must be a string"
         ), "Did not raise TypeError"
@@ -17,12 +21,50 @@ class TestLoadConfig:
         """test for error feedback on file not found"""
         bad_input = "src/superman.yaml"
         with pytest.raises(Exception) as e_info:
-            preprocessing.load_config(bad_input)
+            load_config(bad_input)
         assert (
             str(e_info.value.args[1]) == "No such file or directory"
         ), "Did not raise file not found error"
 
     def test_return_dict(self):
         assert (
-            type(preprocessing.load_config("src/config.yaml")) is dict
+            type(load_config("src/config.yaml")) is dict
         ), "output is not <class 'dict'>"
+
+
+class TestRemoveBlankRows:
+    def test_blank_rows_removed(self):
+        """test that blank rows are removed"""
+        series_with_empty_row = Series([1.0, "", 3.0])
+        expected_outcome = Series([1.0, 3.0])
+        actual = remove_blank_rows(series_with_empty_row)
+        actual_reindexed = actual.reset_index(drop=True)
+        assert all(
+            actual_reindexed == expected_outcome
+        ), "function does not remove blank rows"
+
+    def test_return_series(self):
+        """test that function returns a Series"""
+        actual = remove_blank_rows(Series([1.0, "", 3.0]))
+        assert (
+            type(actual) is Series
+        ), "output is not <class 'pandas.core.series.Series'>"
+
+
+class TestReplaceBlanks:
+    def test_blank_replacement(self):
+        """test replace blanks with NaN"""
+        series_with_empty_row = Series([1.0, "", 3.0])
+        actual = _replace_blanks(series_with_empty_row)
+        assert np.isnan(actual[1]), "did not replace blank with NaN"
+
+    def test_return_series(self):
+        """test that function returns a Series"""
+        actual = remove_blank_rows(Series([1.0, "", 3.0]))
+        assert (
+            type(actual) is Series
+        ), "output is not <class 'pandas.core.series.Series'>"
+
+
+if __name__ == "__main__":
+    unittest.main()
